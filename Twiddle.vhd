20e4586f2bd2c52445ae0f1a9fc0de95317cd858
Coolwhiz1 <ccusi001@gmail.com>
2019-04-29 20:01:20 -0700

    Update Twiddle.vhd

--- Twiddle.vhd
+++ Twiddle.vhd
@@ -37,34 +37,40 @@ rst_in : in std_logic;
 )
 end twiddle;
 
-constant TWR0   : integer := x"7fff";
-constant TWR1   : integer := x"7d89";
-constant TWR2   : integer := x"7641";
-constant TWR3   : integer := x"6a6d";
-constant TWR4   : integer := x"5a82";
-constant TWR5   : integer := x"471c";
-constant TWR6   : integer := x"30fb";
-constant TWR7   : integer := x"18f9";
-constant TWR8   : integer := x"0000";
-constant TWR9   : integer := x"e707";
-constant TWR10   : integer := x"cf05";
-constant TWR11   : integer := x"b8e4";
-constant TWR12   : integer := x"a57e";
-constant TWR13   : integer := x"9593";
-constant TWR14   : integer := x"89bf";
-constant TWR15   : integer := x"8277";
+constant TWR0   : std_logic_vector(7 downto 0) := x"80";
+constant TWR1   : std_logic_vector(7 downto 0) := x"7E";
+constant TWR2   : std_logic_vector(7 downto 0) := x"76";
+constant TWR3   : std_logic_vector(7 downto 0) := x"6A";
+constant TWR4   : std_logic_vector(7 downto 0) := x"5B";
+constant TWR5   : std_logic_vector(7 downto 0) := x"47";
+constant TWR6   : std_logic_vector(7 downto 0) := x"31";
+constant TWR7   : std_logic_vector(7 downto 0) := x"19";
+constant TWR8   : std_logic_vector(7 downto 0) := x"00";
+constant TWR9   : std_logic_vector(7 downto 0) := x"E7";
+constant TWR10   : std_logic_vector(7 downto 0) := x"CF";
+constant TWR11   : std_logic_vector(7 downto 0) := x"B9";
+constant TWR12   : std_logic_vector(7 downto 0) := x"A5";
+constant TWR13   : std_logic_vector(7 downto 0) := x"96";
+constant TWR14   : std_logic_vector(7 downto 0) := x"8A";
+constant TWR15   : std_logic_vector(7 downto 0) := x"82";
 
-constant TWI0   : integer := x"0000";
-constant TWI1   : integer := x"1859";
-constant TWI2   : integer := x"30fb";
-constant TWI3   : integer := x"471c";
-constant TWI4   : integer := x"5a82";
-constant TWI5   : integer := x"6a6d";
-constant TWI6   : integer := x"7641";
-constant TWI7   : integer := x"7d89";
-constant TWI2   : integer := x"7fff";
-constant TWI2   : integer := x"7d89";
-constant TWI2   : integer := x"7641";
+constant TWI0   : std_logic_vector(7 downto 0) := x"00";
+constant TWI1   : std_logic_vector(7 downto 0) := x"19";
+constant TWI2   : std_logic_vector(7 downto 0) := x"31";
+constant TWI3   : std_logic_vector(7 downto 0) := x"47";
+constant TWI4   : std_logic_vector(7 downto 0) := x"5B";
+constant TWI5   : std_logic_vector(7 downto 0) := x"6A";
+constant TWI6   : std_logic_vector(7 downto 0) := x"76";
+constant TWI7   : std_logic_vector(7 downto 0) := x"7E";
+constant TWI8   : std_logic_vector(7 downto 0) := x"80";
+constant TWI9   : std_logic_vector(7 downto 0) := x"7E";
+constant TWI10   : std_logic_vector(7 downto 0) := x"76";
+constant TWI11   : std_logic_vector(7 downto 0) := x"6A";
+constant TWI12   : std_logic_vector(7 downto 0) := x"5B";
+constant TWI13   : std_logic_vector(7 downto 0) := x"47";
+constant TWI14   : std_logic_vector(7 downto 0) := x"31";
+constant TWI15   : std_logic_vector(7 downto 0) := x"19";
+
 artitechture rtl of twiddle is
 
 begin
