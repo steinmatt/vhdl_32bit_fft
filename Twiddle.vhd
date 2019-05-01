0c242be0ded618cc8e672a8acedc4ec0446b0777
Coolwhiz1 <ccusi001@gmail.com>
2019-05-01 14:12:02 -0700

    GOTEM SIGNED SHIT

--- Twiddle.vhd
+++ Twiddle.vhd
@@ -18,70 +18,69 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_unsigned.all;
+use ieee.std_logic_signed.all;
 use ieee.std_logic_arith.all;
+use in_out_matrix.all;
 
 
 entity twiddle is
 port (
 -- Inputs
-real_a, real_b, : in std_logic_vector(7 downto 0);
-img_a, img_b, : in std_logic_vector(7 downto 0);
 
 -- Outputs
-twiddle_real_a  :out std_logic_vector(7 downto 0);
-twiddle_img_a   :out std_logic_vector(7 downto 0);
+twiddle_real  :out tf_array(0 to 15);
+twiddle_img   :out tf_array(0 to 15);
 
 -- Reset
 rst_in : in std_logic;
 )
 end twiddle;
 
-constant TWR0   : std_logic_vector(7 downto 0) := x"7fff";
-constant TWR1   : std_logic_vector(7 downto 0) := x"7d8a";
-constant TWR2   : std_logic_vector(7 downto 0) := x"7641";
-constant TWR3   : std_logic_vector(7 downto 0) := x"6A6d";
-constant TWR4   : std_logic_vector(7 downto 0) := x"5B82";
-constant TWR5   : std_logic_vector(7 downto 0) := x"4721";
-constant TWR6   : std_logic_vector(7 downto 0) := x"30FB";
-constant TWR7   : std_logic_vector(7 downto 0) := x"18F9";
-constant TWR8   : std_logic_vector(7 downto 0) := x"0000";
-constant TWR9   : std_logic_vector(7 downto 0) := x"E707";
-constant TWR10   : std_logic_vector(7 downto 0) := x"CF05";
-constant TWR11   : std_logic_vector(7 downto 0) := x"B8E4";
-constant TWR12   : std_logic_vector(7 downto 0) := x"A57E";
-constant TWR13   : std_logic_vector(7 downto 0) := x"9593";
-constant TWR14   : std_logic_vector(7 downto 0) := x"89BF";
-constant TWR15   : std_logic_vector(7 downto 0) := x"8279";
+constant TWR0   : signed(7 downto 0) := x"7fff";
+constant TWR1   : signed(7 downto 0) := x"7d8a";
+constant TWR2   : signed(7 downto 0) := x"7641";
+constant TWR3   : signed(7 downto 0) := x"6A6d";
+constant TWR4   : signed(7 downto 0) := x"5B82";
+constant TWR5   : signed(7 downto 0) := x"4721";
+constant TWR6   : signed(7 downto 0) := x"30FB";
+constant TWR7   : signed(7 downto 0) := x"18F9";
+constant TWR8   : signed(7 downto 0) := x"0000";
+constant TWR9   : signed(7 downto 0) := x"E707";
+constant TWR10   : signed(7 downto 0) := x"CF05";
+constant TWR11   : signed(7 downto 0) := x"B8E4";
+constant TWR12   : signed(7 downto 0) := x"A57E";
+constant TWR13   : signed(7 downto 0) := x"9593";
+constant TWR14   : signed(7 downto 0) := x"89BF";
+constant TWR15   : signed(7 downto 0) := x"8279";
 
-constant TWI0   : std_logic_vector(7 downto 0) := x"0000";
-constant TWI1   : std_logic_vector(7 downto 0) := x"18F9";
-constant TWI2   : std_logic_vector(7 downto 0) := x"30FB";
-constant TWI3   : std_logic_vector(7 downto 0) := x"471C";
-constant TWI4   : std_logic_vector(7 downto 0) := x"5A82";
-constant TWI5   : std_logic_vector(7 downto 0) := x"6A6D";
-constant TWI6   : std_logic_vector(7 downto 0) := x"7641";
-constant TWI7   : std_logic_vector(7 downto 0) := x"7D8A";
-constant TWI8   : std_logic_vector(7 downto 0) := x"7FFF";
-constant TWI9   : std_logic_vector(7 downto 0) := x"7D8A";
-constant TWI10   : std_logic_vector(7 downto 0) := x"7641";
-constant TWI11   : std_logic_vector(7 downto 0) := x"6A6D";
-constant TWI12   : std_logic_vector(7 downto 0) := x"5A82";
-constant TWI13   : std_logic_vector(7 downto 0) := x"471C";
-constant TWI14   : std_logic_vector(7 downto 0) := x"30FB";
-constant TWI15   : std_logic_vector(7 downto 0) := x"18F9";
+constant TWI0   : signed(7 downto 0) := x"0000";
+constant TWI1   : signed(7 downto 0) := x"18F9";
+constant TWI2   : signed(7 downto 0) := x"30FB";
+constant TWI3   : signed(7 downto 0) := x"471C";
+constant TWI4   : signed(7 downto 0) := x"5A82";
+constant TWI5   : signed(7 downto 0) := x"6A6D";
+constant TWI6   : signed(7 downto 0) := x"7641";
+constant TWI7   : signed(7 downto 0) := x"7D8A";
+constant TWI8   : signed(7 downto 0) := x"7FFF";
+constant TWI9   : signed(7 downto 0) := x"7D8A";
+constant TWI10   : signed(7 downto 0) := x"7641";
+constant TWI11   : signed(7 downto 0) := x"6A6D";
+constant TWI12   : signed(7 downto 0) := x"5A82";
+constant TWI13   : signed(7 downto 0) := x"471C";
+constant TWI14   : signed(7 downto 0) := x"30FB";
+constant TWI15   : signed(7 downto 0) := x"18F9";
 
 
 artitechture rtl of twiddle is
 
 begin
 
-	process (real_a, real_b, img_a, img_b, rst_in )
+	process (rst_in)
 
 		begin
 			if (rst_in <= '0') then
-			out_real_a <= unsigned(real_a) - unsigned(real_b);
-			out_img_a  <= unsigned(img_a)  - unsigned(img_b);
+			out_real_a <= signed(real_a) - signed(real_b);
+			out_img_a  <= signed(img_a)  - signed(img_b);
 			--rborrow_out <= ((not real_a) and ( real_b or rborrow_in)) or ( real_a and rborrow_in));
 			--iborrow_out <= ((not img_a) and ( img_b or iborrow_in)) or ( img_a and iborrow_in));
 			
