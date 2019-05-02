26dd1fda30e21a3da83270a23fd71ffb1e92240e
DESKTOP-PFBBH8V\msmat <msmatt1@hotmail.com>
2019-05-02 13:58:02 -0700

    Update Twiddle.vhd

--- Twiddle.vhd
+++ Twiddle.vhd
@@ -23,59 +23,61 @@ use in_out_matrix.all;
 
 
 entity twiddle is
-port (
--- Inputs
+	port (
+	-- Inputs
 
--- Outputs
-twiddle_real  :out tf_array(0 to 15);
-twiddle_img   :out tf_array(0 to 15);
+	-- Outputs
+	twiddle_real  :out tf_array(0 to 15);
+	twiddle_img   :out tf_array(0 to 15);
 
--- Reset
-rst_in : in std_logic
-);
+	-- Reset
+	rst_in : in std_logic
+	);
 end twiddle;
 
+architecture rtl of twiddle is
+
 --Real Twiddle values
-constant TWR0   : signed(7 downto 0) := x"7fff";
-constant TWR1   : signed(7 downto 0) := x"7d8a";
-constant TWR2   : signed(7 downto 0) := x"7641";
-constant TWR3   : signed(7 downto 0) := x"6A6d";
-constant TWR4   : signed(7 downto 0) := x"5B82";
-constant TWR5   : signed(7 downto 0) := x"4721";
-constant TWR6   : signed(7 downto 0) := x"30FB";
-constant TWR7   : signed(7 downto 0) := x"18F9";
-constant TWR8   : signed(7 downto 0) := x"0000";
-constant TWR9   : signed(7 downto 0) := x"E707";
-constant TWR10   : signed(7 downto 0) := x"CF05";
-constant TWR11   : signed(7 downto 0) := x"B8E4";
-constant TWR12   : signed(7 downto 0) := x"A57E";
-constant TWR13   : signed(7 downto 0) := x"9593";
-constant TWR14   : signed(7 downto 0) := x"89BF";
-constant TWR15   : signed(7 downto 0) := x"8279";
+constant TWR0    : signed(15 downto 0) := x"7fff";
+constant TWR1    : signed(15 downto 0) := x"7d8a";
+constant TWR2    : signed(15 downto 0) := x"7641";
+constant TWR3    : signed(15 downto 0) := x"6A6d";
+constant TWR4    : signed(15 downto 0) := x"5B82";
+constant TWR5    : signed(15 downto 0) := x"4721";
+constant TWR6    : signed(15 downto 0) := x"30FB";
+constant TWR7    : signed(15 downto 0) := x"18F9";
+constant TWR8    : signed(15 downto 0) := x"0000";
+constant TWR9    : signed(15 downto 0) := x"E707";
+constant TWR10   : signed(15 downto 0) := x"CF05";
+constant TWR11   : signed(15 downto 0) := x"B8E4";
+constant TWR12   : signed(15 downto 0) := x"A57E";
+constant TWR13   : signed(15 downto 0) := x"9593";
+constant TWR14   : signed(15 downto 0) := x"89BF";
+constant TWR15   : signed(15 downto 0) := x"8279";
 
 --RESET constant
-constant MAL	 : signed(7 downto 0) := x"0000";
+constant MAL	 : signed(15 downto 0) := x"0000";
 
 --Imaginary Twiddle factors
-constant TWI0   : signed(7 downto 0) := x"0000";
-constant TWI1   : signed(7 downto 0) := x"18F9";
-constant TWI2   : signed(7 downto 0) := x"30FB";
-constant TWI3   : signed(7 downto 0) := x"471C";
-constant TWI4   : signed(7 downto 0) := x"5A82";
-constant TWI5   : signed(7 downto 0) := x"6A6D";
-constant TWI6   : signed(7 downto 0) := x"7641";
-constant TWI7   : signed(7 downto 0) := x"7D8A";
-constant TWI8   : signed(7 downto 0) := x"7FFF";
-constant TWI9   : signed(7 downto 0) := x"7D8A";
-constant TWI10   : signed(7 downto 0) := x"7641";
-constant TWI11   : signed(7 downto 0) := x"6A6D";
-constant TWI12   : signed(7 downto 0) := x"5A82";
-constant TWI13   : signed(7 downto 0) := x"471C";
-constant TWI14   : signed(7 downto 0) := x"30FB";
-constant TWI15   : signed(7 downto 0) := x"18F9";
+constant TWI0    : signed(15 downto 0) := x"0000";
+constant TWI1    : signed(15 downto 0) := x"18F9";
+constant TWI2    : signed(15 downto 0) := x"30FB";
+constant TWI3    : signed(15 downto 0) := x"471C";
+constant TWI4    : signed(15 downto 0) := x"5A82";
+constant TWI5    : signed(15 downto 0) := x"6A6D";
+constant TWI6    : signed(15 downto 0) := x"7641";
+constant TWI7    : signed(15 downto 0) := x"7D8A";
+constant TWI8    : signed(15 downto 0) := x"7FFF";
+constant TWI9    : signed(15 downto 0) := x"7D8A";
+constant TWI10   : signed(15 downto 0) := x"7641";
+constant TWI11   : signed(15 downto 0) := x"6A6D";
+constant TWI12   : signed(15 downto 0) := x"5A82";
+constant TWI13   : signed(15 downto 0) := x"471C";
+constant TWI14   : signed(15 downto 0) := x"30FB";
+constant TWI15   : signed(15 downto 0) := x"18F9";
 
 
-artitechture rtl of twiddle is
+
 
 begin
 
